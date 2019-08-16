47fd0227de127a94fb6573202543ff50e9ea7998
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-08-17 01:14:29 +0200

    Add project files.

---
+++ Game1/Tanky.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class Tanky
+    {
+        private const int Size = 32;
+        private Texture2D walkAnim;
+
+        public Tanky()
+        {
+            Width = Size;
+            Height = Size;
+            Top = 400;
+        }
+
+        public Vector2 Speed { get; set; }
+
+        public int Height { get; }
+
+        public int Width { get; }
+
+        public float Top { get; set; }
+
+        public int WalkIndex { get; private set; }
+        public float Left { get; set; }
+
+        public void Load(ContentManager contentManager)
+        {
+            walkAnim = contentManager.Load<Texture2D>("Tanky");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var destinationRectangle = new Rectangle((int) Left, (int) Top, Width, Height);
+            var sourceRectangle = TextureMixin.GetTile(WalkIndex, 0, Size, Size);
+
+            spriteBatch.Draw(walkAnim, destinationRectangle, sourceRectangle, Color.White);
+
+            NextFrame();
+        }
+
+        private void NextFrame()
+        {
+            if (WalkIndex == 7)
+            {
+                WalkIndex = 0;
+            }
+            else
+            {
+                WalkIndex++;
+            }
+        }
+    }
+}
---
+++ Game1/TextureMixin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Game1
+{
+    public static class TextureMixin
+    {
+        public static Rectangle GetTile(int column, int row, int width, int height)
+        {
+            return new Rectangle(width * column, height * row, width, height);
+        }
+    }
+}
